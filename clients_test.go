dca6636d7953eb042ad6f543308a0f4b5f011d8b
Scott Clasen <scott@heroku.com>
2014-10-18 23:07:20 -0700

    fmt

--- clients_test.go
+++ clients_test.go
@@ -7,14 +7,12 @@ import (
 
 func TestListWorkflowTypes(t *testing.T) {
 
-	request := ListWorkflowTypesRequest{
-		Domain:             "swf4go",
-		RegistrationStatus: "REGISTERED",
-	}
-
 	client := NewClient(MustGetenv("AWS_ACCESS_KEY_ID"), MustGetenv("AWS_SECRET_ACCESS_KEY"), USEast1)
 
-	resp, err := client.ListWorkflowTypes(request)
+	resp, err := client.ListWorkflowTypes(ListWorkflowTypesRequest{
+		Domain:             "swf4go",
+		RegistrationStatus: "REGISTERED",
+	})
 
 	if err != nil {
 		t.Fail()
