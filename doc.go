031f7b5b31593fb98eb920f36cf9b7952d5bd7ad
Scott Clasen <scott@heroku.com>
2014-11-07 11:26:38 -0800

    start of decent docs

--- doc.go
+++ doc.go
@@ -1,10 +1,10 @@
+package swf
 /*
 Package swf provides a full implementation of a client api for Amazon Simple Workflow Service
 http://docs.aws.amazon.com/amazonswf/latest/apireference/
 
 In addition it provides a basic facility for modeling swf workflows as FSMs (finite state machines), as well as
 implementations of pollers for both decision and activty tasks.
-
 
 Client
 
@@ -51,7 +51,7 @@ Substituting the relevant swf/swf4go concepts, we get
 
 */
 
-package swf
+
 
 /*
 
